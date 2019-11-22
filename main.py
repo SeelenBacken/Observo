65483011099cf02061a28f06cc8e809d1bcad887
SoulBack <mail@soulback.de>
2019-11-22 19:17:26 +0100

    Added Basic Bot Structure (Terminator not used right now)

--- main.py
+++ main.py
@@ -1,17 +1,55 @@
 from apprentice import Apprentice
+from terminator import Terminator
+from confuzius import Confuzius
+import discord
+import datetime
 
-
-class Observo:
-    '''A bot for User analysis or some other stuff I don't know about yet.
+'''A bot for User analysis or some other stuff I don't know about yet.
 
     He runs on a local server and is private use only currently. As soon as the project is ready he becomes available
     for everyone, if possible ofc. (ToC or smth, idk)
     '''
+configs = {
+    'name': 'Observo',
+    'prefix': 'Osrv'
+}
 
-    def __init__(self):
-        discordObserver = Apprentice()
-        print(discordObserver.test())
+print('------Welcome to Observo------')
+# term = Terminator()
+conf = Confuzius()
+# discordObserver = Apprentice()
+
+client = discord.Client()
 
 
-if __name__ == '__main__':
-    ObservoBot = Observo()
+@client.event
+async def on_ready():
+    print(getTimeString() + 'Logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    messageList = getMessageList(message)
+    if message.author == client.user:
+        return
+    elif message.content == '$test':
+        await message.channel.send('Erfolg du Fotze')
+    else:
+        print('')
+
+def getTimeString():
+    now = datetime.datetime.now()
+    return '[{}.{}.{}-{}:{}:{}] '.format(now.year, now.month, now.day, now.hour, now.minute, now.second)
+
+def getMessageList(message):
+    return {
+        'guildID': message.guild.id,
+        'guildName': message.guild.name,
+        'channelID': message.channel.id,
+        'channelName': message.channel.name,
+        'roleID': message.author.roles[-1].id,
+        'roleName': message.author.roles[-1].name,
+        'datetime': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    }
+
+
+client.run(conf.getToken())
